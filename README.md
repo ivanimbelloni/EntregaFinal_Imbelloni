c21e28bcb39560610c8c1ecd46e628e41d6098df
Ivan Gustavo Imbelloni <134248767+ivanimbelloni@users.noreply.github.com>
2023-07-06 05:24:15 -0300

    Copia pruebas (#1)
    * agregar node y scss
    
    * agregar node y sass
    
    * modifique imgs
    
    * edicion img
    
    * arreglos y mejora en el nav-link
    
    * arreglos en el footer y detalles del css

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pre_entrega2_Imbelloni
+
+hola, si puede revisar mi brand copia_pruebas para saber si instale bien el sass y node, gracias 😁
---
+++ javascript/ContactBtnSubmit.js
@@ -0,0 +1,9 @@
+const btnSubmit = document.querySelector("#contact-submit");
+
+
+function alertMessage(){
+    alert("Sus datos se han enviado");
+}
+
+
+btnSubmit.onclick(alertMessage)
